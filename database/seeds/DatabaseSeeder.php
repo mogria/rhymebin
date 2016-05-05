9e4defb2d8ca534679b5777514bc21771307c631
Mogria <m0gr14@gmail.com>
2016-05-05 23:16:44 +0200

    add default user seed for database & run script now installs all dependencies & seeds database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+
+        // only seed the users table once because their names must be unique
+        $user = DB::table('users')->where('name', 'Mogria')->first();
+        if($user === null) {
+            echo "RhymeBin: creating default user 'Mogria' with password 'test'\n";
+            DB::table('users')->insert([
+                'name' => 'Mogria',
+                'email' => 'm0gr14@gmail.com',
+                'password' => Hash::make('testtest')
+            ]);
+        }
     }
 }
--- run.sh
+++ run.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
+
+SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$SOURCE_DIR"
+
+echo "RhymeBin: Running composer install to grab lumen and other PHP dependencies"
+composer install
+
+echo "RhymeBin: Running npm install to grab bower & gulp"
+npm install
+
+echo "RhymeBin: Running bower install to grab client side JavaScript libraries"
+bower install
+
+if [ ! -f '.env' ]; then
+    echo "RhymeBin: Copying default environment configuration (database connection, memcached configuration, etc.)"
+    cp .env.example .env
+fi
+
+echo "RhymeBin: Running gulp for the first time to compile assets ..."
+gulp
+
+echo "RhymeBin: Seeding the database with php artisan db:seed"
+php artisan db:seed
+
+echo "RhymeBin: Running gulp watch ... "
+gulp watch &
+
+echo "RhymeBin: Running PHPs own webserver on 127.0.0.1:1337 WEBROOT=public/  ... "
 php -S 127.0.0.1:1337 -t "$(dirname "${BASH_SOURCE[0]}")/public"
