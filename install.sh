17d6fca6a57a5ce4edb2d940bfb6f5b04139ead7
Mogria <m0gr14@gmail.com>
2016-05-12 18:04:45 +0200

    split run script into install script and serve script

--- install.sh
+++ install.sh
@@ -34,14 +34,5 @@ if [ ! -f '.env' ]; then
     cp .env.example .env
 fi
 
-echo "RhymeBin: Running gulp for the first time to compile assets ..."
-gulp
-
 echo "RhymeBin: Seeding the database with php artisan db:seed"
 php artisan db:seed
-
-echo "RhymeBin: Running gulp watch ... "
-gulp watch &
-
-echo "RhymeBin: Running PHPs own webserver on 127.0.0.1:1337 WEBROOT=public/  ... "
-php -S 127.0.0.1:1337 -t "$(dirname "${BASH_SOURCE[0]}")/public"
---
+++ serve.sh
@@ -0,0 +1,8 @@
+echo "RhymeBin: Running gulp for the first time to compile assets ..."
+gulp
+
+echo "RhymeBin: Running gulp watch ... "
+gulp watch &
+
+echo "RhymeBin: Running PHPs own webserver on 127.0.0.1:1337 WEBROOT=public/  ... "
+php -S 127.0.0.1:1337 -t "$(dirname "${BASH_SOURCE[0]}")/public"
