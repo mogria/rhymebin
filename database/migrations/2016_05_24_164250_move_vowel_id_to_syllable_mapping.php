e86337ceb0b1920dd1bc6257481eb682bc3ae669
Mogria <m0gr14@gmail.com>
2016-05-24 18:54:03 +0200

    move the vowel_id column to syllable_mappings, because the same syllables will not always be spoken the same

---
+++ database/migrations/2016_05_24_164250_move_vowel_id_to_syllable_mapping.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MoveVowelIdToSyllableMapping extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('syllables', function(Blueprint $table) {
+            $table->dropForeign('syllables_vowel_id_foreign');
+            $table->dropColumn('vowel_id');
+        });
+
+        Schema::table('syllable_mappings', function(Blueprint $table) {
+            $table->integer('vowel_id')->unsigned();
+            $table->foreign('vowel_id')->references('id')->on('vowels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('syllables', function(Blueprint $table) {
+            $table->integer('vowel_id')->unsigned();
+            $table->foreign('vowel_id')->references('id')->on('vowels');
+        });
+
+        Schema::table('syllable_mappings', function(Blueprint $table) {
+            $table->dropForeign('syllables_vowel_id_foreign');
+            $table->dropColumn('vowel_id');
+        });
+    }
+}
