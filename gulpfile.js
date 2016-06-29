4fca01790be568b1b7ac96866569abeab1b08c5a
Mogria <m0gr14@gmail.com>
2016-06-29 18:57:29 +0200

    fix gulpfile for newer versions of elixir. Only concatenate javascript files instead of running them throgh webpack

--- gulpfile.js
+++ gulpfile.js
@@ -4,42 +4,50 @@ var gulp = require('gulp'),
 
 elixir.config.sourcemaps = true;
 
+function component(name) {
+    return './vendor/bower_components/' + name + '.js';
+}
+
+function js(name) {
+    return './resources/assets/js/' + name + '.js';
+}
+
 elixir(function(mix) {
-    var scripts = [ './vendor/bower_components/angular/angular.js'
-                  , './vendor/bower_components/angular-resource/angular-resource.js'
-                  , './vendor/bower_components/angular-sanitize/angular-sanitize.js'
-                  , './vendor/bower_components/angular-ui-router/release/angular-ui-router.js'
-                  , './vendor/bower_components/jquery/dist/jquery.js' // bootstraps javascript requires jQuery ...
-                  , './vendor/bower_components/bootstrap-sass/assets/javascripts/bootstrap/collapse.js' 
-                  , './vendor/bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'
-                  , './vendor/bower_components/satellizer/satellizer.js'
-                  , 'controllers.js'
-                  , 'controllers/AboutController.js'
-                  , 'controllers/HomeController.js'
-                  , 'controllers/LoginController.js'
-                  , 'controllers/NavController.js'
-                  , 'controllers/RegisterController.js'
-                  , 'controllers/WordEntryController.js'
-                  , 'controllers/WordIndexController.js'
-                  , 'controllers/WordShowController.js'
-                  , 'directives.js'
-                  , 'directives/errorsFor.js'
-                  , 'directives/remoteValidate.js'
-                  , 'directives/vowelExample.js'
-                  , 'directives/vowelSelection.js'
-                  , 'filters.js'
-                  , 'filters/vowelExample.js'
-                  , 'services.js'
-                  , 'services/authService.js'
-                  , 'services/httpErrorHandlerService.js'
-                  , 'services/Language.js'
-                  , 'services/Vowel.js'
-                  , 'services/Word.js'
-                  , 'app.js' // needs to be last
+    var scripts = [ component('angular/angular')
+                  , component('angular-resource/angular-resource')
+                  , component('angular-sanitize/angular-sanitize')
+                  , component('angular-ui-router/release/angular-ui-router')
+                  , component('jquery/dist/jquery') // bootstraps javascript requires jQuery ...
+                  , component('bootstrap-sass/assets/javascripts/bootstrap/collapse' )
+                  , component('bootstrap-sass/assets/javascripts/bootstrap')
+                  , component('satellizer/satellizer')
+                  , js('controllers')
+                  , js('controllers/AboutController')
+                  , js('controllers/HomeController')
+                  , js('controllers/LoginController')
+                  , js('controllers/NavController')
+                  , js('controllers/RegisterController')
+                  , js('controllers/WordEntryController')
+                  , js('controllers/WordIndexController')
+                  , js('controllers/WordShowController')
+                  , js('directives')
+                  , js('directives/errorsFor')
+                  , js('directives/remoteValidate')
+                  , js('directives/vowelExample')
+                  , js('directives/vowelSelection')
+                  , js('filters')
+                  , js('filters/vowelExample')
+                  , js('services')
+                  , js('services/authService')
+                  , js('services/httpErrorHandlerService')
+                  , js('services/Language')
+                  , js('services/Vowel')
+                  , js('services/Word')
+                  , js('app') // needs to be last
                   ];
 
 
-    mix.scripts(scripts, 'public/js/app.js');
+    mix.combine(scripts, 'public/js/app.js');
     if(!util.env.production) {
         // create the LiveReload script
         mix.scripts(['./vendor/bower_components/livereload-js/dist/livereload.js'], 'public/js/livereload.js');
