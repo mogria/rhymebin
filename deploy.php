67f3e1ace072895fa636c12915a91cbbb7416a39
Mogria <m0gr14@gmail.com>
2016-08-28 15:39:01 +0200

    update deploy.php, so assets get compiled automatically by gulp when deploying

--- deploy.php
+++ deploy.php
@@ -19,3 +19,44 @@ set('copy_dirs', ['vendor']);
 before('deploy:vendors', 'deploy:copy_dirs');
 
 set('keep_releases', 10); // keep the last 10 releases
+
+env('bin/npm', function() {
+  return run('which npm')->toString();
+});
+
+env('bin/bower', function() {
+  return run('which bower')->toString();
+});
+
+env('bin/gulp', function() {
+  return run('which gulp')->toString();
+});
+
+task('npm:install', function() {
+  $output = run('cd {{deploy_path}}/current && {{bin/npm}} install --production');
+  writeln('<info>' . $output . '</info>');
+})->desc('Install npm dependencies');
+
+task('bower:install', function() {
+  $output = run('cd {{deploy_path}}/current && {{bin/bower}} install --production');
+  writeln('<info>' . $output . '</info>');
+})->desc('Install bower dependencies');
+
+task('gulp:all', function() {
+  $output = run('cd {{deploy_path}}/current && {{bin/gulp}} --production');
+  writeln('<info>' . $output . '</info>');
+})->desc('Run all gulp tasks');
+
+task('compile:assets', ['npm:install', 'bower:install', 'gulp:all']);
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'compile:assets',
+    'deploy:writable',
+    'deploy:symlink',
+    'cleanup'
+])->desc('Deploy your project');
