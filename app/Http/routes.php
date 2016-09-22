855fc01f2b7d44e06ac3ef16e03da83b40c4fefc
Mogria <m0gr14@gmail.com>
2016-09-23 00:20:19 +0200

    vowels dont need authentication anymore

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,30 +19,27 @@ $app->get('/', function() {
     return view('index'); 
 });
 
-// api witch authentication
+// api with authentication
 $app->group(['prefix' => '/api', 'middleware' => 'jwt.auth', 'namespace' => 'App\Http\Controllers'], function() use ($app) {
-    $app->get('/', function () use ($app) { 
-        return $app->version();
-    });
-    
     $app->get('/users', 'UserController@getUsers');
     $app->get('/users/{id}', 'UserController@getUser');
-    
     $app->post('/languages/{language_id}/words', 'WordController@postWords');
-    
-
-    $app->get('/languages/{language_id}/vowels', 'VowelController@getVowels');
-    $app->get('/languages/{language_id}/vowels/{id}', 'VowelController@getVowel');
 });
 
 // api with no authentication
 $app->group(['prefix' => '/api', 'namespace' => 'App\Http\Controllers'], function() use ($app) {
+    $app->get('/', function () use ($app) { 
+        return $app->version();
+    });
 
     $app->get('/languages', 'LanguageController@getLanguages');
     $app->get('/languages/{id}', 'LanguageController@getLanguage');
     $app->get('/languages/{language_id}/words/rhymes', 'WordController@getWordRhymes');
     $app->get('/languages/{language_id}/words/{word_id}', 'WordController@getWord');
     $app->get('/languages/{language_id}/words', 'WordController@getWords');
+    $app->get('/languages/{language_id}/vowels', 'VowelController@getVowels');
+    $app->get('/languages/{language_id}/vowels/{id}', 'VowelController@getVowel');
+
     $app->post('/login', 'SessionController@postLogin');
     $app->post('/logout', 'SessionController@postLogout');
     $app->post('/refresh', 'SessionController@postRefresh');
